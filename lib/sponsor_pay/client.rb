4ceb1b5241b18ebdb07086c7026c66840aa82954
Ancor Cruz <hello@ancorcruz.com>
2013-12-05 13:16:00 +0000

    Added prepare_params specs and method in SponsorPay Client lib

--- lib/sponsor_pay/client.rb
+++ lib/sponsor_pay/client.rb
@@ -1,3 +1,7 @@
+require 'json'
+require 'open-uri'
+require 'digest/sha1'
+
 module SponsorPay
   class Client
     def initialize api_key
@@ -7,5 +11,25 @@ module SponsorPay
     def fetch_offers params
       { "code" => "OK" }
     end
+
+    private
+
+    def prepare_params raw_params
+      normalized_params = {}.tap { |params| raw_params.each { |k, v| params[k] = v.gsub(/\s+/, "") } }
+      sorted_params     = normalized_params.sort
+      pairs_array       = sorted_params.inject([]) { |pairs, param| pairs << param.join("=") }
+      params_str        = pairs_array.join "&"
+      hashkey           = Digest::SHA1.hexdigest(params_str + "&#{@api_key}")
+
+      params_str + "&hashkey=#{hashkey}"
+    end
   end
 end
+
+#private
+#
+#  JSON.parse get_offers params
+#def get_offers raw_params
+#  params = prepare_params raw_params
+#  open("http://api.sponsorpay.com/feed/v1/offers.json?#{params}").read
+#end
--- spec/unit/sponsor_pay_client_spec.rb
+++ spec/unit/sponsor_pay_client_spec.rb
@@ -3,14 +3,37 @@ require 'sponsor_pay/client'
 
 module SponsorPay
   describe Client do
-    let(:api_key) { "e95a21621a1865bcbae3bee89c4d4f84" }
+    let(:api_key)    { "e95a21621a1865bcbae3bee89c4d4f84" }
+    let(:client)     { Client.new api_key }
+    let(:raw_params) {
+      {
+        appid:     "157",
+        uid:        "player1",
+        ip:         "212.45.111.17",
+        locale:     "de",
+        device_id:  "2b6f0cc904d137be2 e1730235f5664094b 831186",
+        ps_time:    "1312211903",
+        pub0:       "campaign2",
+        page:       "2",
+        timestamp:  "1312553361",
+      }
+    }
 
     describe "#fetch_offers" do
       it "receives an OK response" do
-        client = Client.new api_key
-        params = {}
-        offers = client.fetch_offers params
+        offers = client.fetch_offers raw_params
         offers["code"].should be_eql "OK"
+      end
+    end
+
+    describe "#prepare_params" do
+      class Client
+        public :prepare_params
+      end
+
+      it "returns a params string with the hashkey at the end" do
+        params = client.prepare_params raw_params
+        params.should be_eql "appid=157&device_id=2b6f0cc904d137be2e1730235f5664094b831186&ip=212.45.111.17&locale=de&page=2&ps_time=1312211903&pub0=campaign2&timestamp=1312553361&uid=player1&hashkey=7a2b1604c03d46eec1ecd4a686787b75dd693c4d"
       end
     end
   end
